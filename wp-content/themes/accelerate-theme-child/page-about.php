b3c3994d79b75315d296668d253cce1d2c589a8a
rjamielynn <rjamielynn@gmail.com>
2014-12-29 16:28:53 -0500

    custom pages

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -4,8 +4,17 @@ Template Name: About
 */
 
 get_header(); ?>
-	
-	<div id="primary" class="site-content" id="contact_form">
+	<section class="home-page">
+	<div class="site-content">
+		<?php while ( have_posts() ) : the_post(); ?>
+			<div class='homepage-hero'>
+				<h2>About Accelerate</h2>
+			</div>
+		<?php endwhile; // end of the loop. ?>
+	</div><!-- .container -->
+</section><!-- .home-page -->
+
+	<div id="primary" class="site-content">
 		<div id="content" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
 				<h2><?php the_title(); ?></h2>
---
+++ wp-content/themes/accelerate-theme-child/single-about.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * The template for displaying about
+ *
+ *
+ * @package WordPress
+ * @subpackage Accelerate_Theme
+ * @since Accelerate Theme 1.1
+ */
+
+get_header(); ?>
+	<div id="primary" class="site-content">
+		<div id="content" role="main">
+			
+		<?php while ( have_posts() ) : the_post(); 
+			$size = "thumbnail";
+			$services = get_field('services');
+			$image = get_field('image'); ?>
+
+			<article class="about">
+				<div class="about_images">
+					<?php if($image) { ?>
+					<?php echo wp_get_attachment_image( $image, $size ); ?>
+					<?php } ?>
+				</div>
+				
+				<aside>
+   					<h2><?php the_title(); ?></h2>
+   					<h5><?php echo $services; ?></h5>
+   					<?php the_content(); ?>
+				</aside>
+			</article>
+
+
+		<?php endwhile; // end of the loop. ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+<?php get_footer(); ?>
+
+
+
+
+
